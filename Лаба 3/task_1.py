54677ebde70517ec036f66bbc9882ab3f0277627
Happyestr <77533958+Happyestr@users.noreply.github.com>
2023-10-23 18:59:56 +0300

    Add files via upload

---
+++ Лаба 3/task_1.py
@@ -0,0 +1,15 @@
+# TODO Напишите функцию для поиска индекса товара
+def function(items_list, item_to_find):
+    if(item_to_find in items_list):
+        return items_list.index(item_to_find)
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = function(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
---
+++ Лаба 3/task_2.py
@@ -0,0 +1,10 @@
+# TODO Напишите функцию find_common_participants
+def find_common_participants(participants_first_group, participants_second_group, sep=','):
+    return sorted(set(participants_first_group.split(sep)).intersection(participants_second_group.split(sep)))
+
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
+print(find_common_participants(participants_first_group, participants_second_group, sep='|'))
